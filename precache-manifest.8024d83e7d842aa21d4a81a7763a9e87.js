7f0931ff8d552a779307d7ea0992684b2d3f27f5
kirito22037 <kirito22037@gmail.com>
2020-07-24 01:35:40 +0530

    Updates

---
+++ precache-manifest.8024d83e7d842aa21d4a81a7763a9e87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1085a9540d0b884cff2019fd137422f2",
+    "url": "/hello-mars-/index.html"
+  },
+  {
+    "revision": "33aaf02e41d603e36b31",
+    "url": "/hello-mars-/static/css/main.ff78729a.chunk.css"
+  },
+  {
+    "revision": "1fb74addcf301151d1b6",
+    "url": "/hello-mars-/static/js/2.55653967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-mars-/static/js/2.55653967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33aaf02e41d603e36b31",
+    "url": "/hello-mars-/static/js/main.92ecce9f.chunk.js"
+  },
+  {
+    "revision": "a1c93a7ad29431bc8668",
+    "url": "/hello-mars-/static/js/runtime-main.37431fe4.js"
+  }
+]);
